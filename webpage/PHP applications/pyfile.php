32a213c8ce1ddd45a909879a445d3cf3f5facf8f
reytorremis <164375035+reytorremis@users.noreply.github.com>
2024-04-04 05:35:23 -0700

    Add files via upload

--- webpage/PHP applications/pyfile.php
+++ webpage/PHP applications/pyfile.php
@@ -1,13 +1,13 @@
 <?php
 //Pythom File For reference
 
-$python = "C:/Users/rey.torrecampo/AppData/Local/Programs/Python/Python38/python.exe";
+$python = "C:/Users/.../python.exe";
 
-$py_db_connection = "C:/Users/rey.torrecampo/PycharmProjects/db_ims/python_db_connection.py";
+$py_db_connection = "C:/.../python_db_connection.py";
 
-$py_file_mailer = "C:/xampp/htdocs/dbims/mailer.py";
+$py_file_mailer = "C:/.../mailer.py";
 
 
-$py_file_run = "C:/Users/rey.torrecampo/PycharmProjects/db_ims/run_manual.py";
+$py_file_run = "C:/../run_manual.py";
 
 ?>
